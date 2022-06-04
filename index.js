933df4b5d8bdd84dd3511e996d18c94fa7f90982
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-06-04 09:28:50 -0300

    Finalizando o alert

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const alert = document.getElementById('button')
-if (alert) {
-  alert.addEventListener('mousedown', () => {
-    alert('Você já foi logado', 'success')
+const alertPlace = document.getElementById('alertPlace');
+ 
+const alert = (message, type) =>{
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+    alertPlace.append(wrapper)
+}
+const alertButton = document.getElementById('button');
+if (alertButton) {
+  alertButton.addEventListener('click', () => {
+    alert('Parabéns, você acabou de efetuar seu login!', 'success')
   })
 }
